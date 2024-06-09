9222098773aaeb3f815afce86ab9ceb337b745cf
Ashenafi Yirgalem <ashenafiyirgalem@gmail.com>
2024-06-09 21:56:38 +0300

    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import numpy as np
 from PIL import Image
 import io
 
+
+# data:image/jpeg;base64,{{ image }}
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,58 +19,57 @@ def index():
     
 @app.route('/home', methods=['POST'])
 def home():
-        # # read the image to be recognised
-        # image_path = request.form.get('imagePath')
-        # # image = cv.imread(image_path)
-        # image = Image.open(image_path)
-        # imageDisplay = Image.open(image_path)
-        # imageDisplayArray = np.array(imageDisplay)
+        # read the image to be recognised
+        image_path = request.form.get('imagePath')
+        # image = cv.imread(image_path)
+        image = Image.open(image_path)
+        imageDisplay = Image.open(image_path)
+        imageDisplayArray = np.array(imageDisplay)
 
-        # # resize the image to match the trained network dimensions
-        # img_resized = image.resize((64, 64))
-        # img_data = np.array(img_resized)
-
-
-        # # Preprocess the test image (normalize pixel values)
-        # test_image = np.expand_dims(img_data, axis=0)
-        # test_image = test_image / 255.0
-
-        # # Make predictions
-        # predictions = model.predict(test_image)
-        # predicted_class_index = np.argmax(predictions)
+        # resize the image to match the trained network dimensions
+        img_resized = image.resize((64, 64))
+        img_data = np.array(img_resized)
 
 
-        # # personal details coresponding to classes
-        # person_details = {
-        #     'abraham_dessie': {'name': 'Abraham Dessie', 'Department': 'Computer Science', 'personality': 'hard worker'},
-        #     'amare_chanie': {'name': 'Amare Chanie', 'Department': 'Computer Science', 'personality': 'humble'},
-        #     'ashenafi_yirgalem': {'name': 'Ashenafi Yirgalem', 'Department': 'Computer Science', 'personality': 'creative'},
-        #     'habtamu_wale': {'name': 'Habtamu Wale', 'Department': 'Computer Science', 'personality': 'leader'},
-        #     'tadla_eshetie': {'name': 'Tadla Eshetie', 'Department': 'Computer Science', 'personality': 'wholesome'}
-        # }
+        # Preprocess the test image (normalize pixel values)
+        test_image = np.expand_dims(img_data, axis=0)
+        test_image = test_image / 255.0
 
-        # # class labels
-        # class_labels = ['abraham_dessie', 'amare_chanie', 'ashenafi_yirgalem','habtamu_wale', 'tadla_eshetie']
+        # Make predictions
+        predictions = model.predict(test_image)
+        predicted_class_index = np.argmax(predictions)
 
-        # # Get the recognized person's details
-        # predicted_class_name = class_labels[predicted_class_index]
-        # recognized_person = person_details.get(predicted_class_name, {})
-        # confidence_score = predictions[0][predicted_class_index] * 100 
 
-        # # Display results
-        # Name = recognized_person.get('name', 'Unknown')
-        # department = recognized_person.get('Department', 'Unknown')
-        # personality = recognized_person.get('personality', 'Unknown')
+        # personal details coresponding to classes
+        person_details = {
+            'abraham_dessie': {'name': 'Abraham Dessie', 'Department': 'Computer Science', 'personality': 'hard worker'},
+            'amare_chanie': {'name': 'Amare Chanie', 'Department': 'Computer Science', 'personality': 'humble'},
+            'ashenafi_yirgalem': {'name': 'Ashenafi Yirgalem', 'Department': 'Computer Science', 'personality': 'creative'},
+            'habtamu_wale': {'name': 'Habtamu Wale', 'Department': 'Computer Science', 'personality': 'leader'},
+            'tadla_eshetie': {'name': 'Tadla Eshetie', 'Department': 'Computer Science', 'personality': 'wholesome'}
+        }
 
-        # # Encode the image as base64
-        # image_64bit = imageDisplayArray.astype(np.uint8)
-        # buffer = io.BytesIO()  # Create an in-memory buffer
-        # Image.fromarray(image_64bit).convert('RGB').save(buffer, format='PNG')
-        # image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        # class labels
+        class_labels = ['abraham_dessie', 'amare_chanie', 'ashenafi_yirgalem','habtamu_wale', 'tadla_eshetie']
 
-        # # return
-        # return render_template('home.html', image=image, name=Name, confidance=f"{confidence_score:.2f}%", Department=department,personality=personality);
-        return render_template('home.html')
+        # Get the recognized person's details
+        predicted_class_name = class_labels[predicted_class_index]
+        recognized_person = person_details.get(predicted_class_name, {})
+        confidence_score = predictions[0][predicted_class_index] * 100 
+
+        # Display results
+        Name = recognized_person.get('name', 'Unknown')
+        department = recognized_person.get('Department', 'Unknown')
+        personality = recognized_person.get('personality', 'Unknown')
+
+        # Encode the image as base64
+        image_64bit = imageDisplayArray.astype(np.uint8)
+        buffer = io.BytesIO()  # Create an in-memory buffer
+        Image.fromarray(image_64bit).convert('RGB').save(buffer, format='PNG')
+        image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+        # return
+        return render_template('home.html', image=image, name=Name, confidance=f"{confidence_score:.2f}%", Department=department,personality=personality);
 
 if __name__ == '__main__':
     app.run()
